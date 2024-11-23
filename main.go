f88663006e3fda404675c1d55ccb44724842c321
SaiSawant1 <saisawant2003@gmail.com>
2024-11-23 22:42:48 +0530

    first code test

--- main.go
+++ main.go
@@ -10,7 +10,17 @@ func main() {
 	{
 		v1.GET("/1", func(c *gin.Context) {
 			c.JSON(200, gin.H{
-				"message": "pong",
+				"code": `
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.linear_model import LinearRegression
+import numpy as np
+
+df = pd.read_csv("Iris.csv")
+
+print("First few rows of the dataset: \n", df.head())
+        `,
 			})
 		})
 	}
