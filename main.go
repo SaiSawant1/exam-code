458881b486c4f094e4991ea5b59f4490072ed74c
SaiSawant1 <saisawant2003@gmail.com>
2024-11-23 23:24:42 +0530

    code 5

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 		v1.GET("/2", aiethics.Lab2Code)
 		v1.GET("/3", aiethics.Lab3Code)
 		v1.GET("/4", aiethics.Lab4Code)
-		v1.GET("/5", aiethics.Lab1Code)
+		v1.GET("/5", aiethics.Lab5Code)
 		v1.GET("/6", aiethics.Lab1Code)
 		v1.GET("/7", aiethics.Lab1Code)
 		v1.GET("/8", aiethics.Lab1Code)
