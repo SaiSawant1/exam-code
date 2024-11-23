56c2f09dbb1f503b24850a34d886d1c22b8947b9
SaiSawant1 <saisawant2003@gmail.com>
2024-11-23 23:01:48 +0530

    first code

--- AI_Ethics/ai_ethics.go
+++ AI_Ethics/ai_ethics.go
@@ -2,7 +2,7 @@ package aiethics
 
 import "github.com/gin-gonic/gin"
 
-var code string = `
+var code1 string = `
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -12,8 +12,39 @@ import numpy as np
 df = pd.read_csv("Iris.csv")
 
 print("First few rows of the dataset: \n", df.head())
+X = df['PetalLengthCm']
+y = df['PetalWidthCm']
+
+plt.figure(figsize=(8,6))
+plt.scatter(X, y, color='blue')
+plt.xlabel('Petal Length')
+plt.ylabel('Petal Width')
+plt.title('Scatter Plot: Petal Length vs Petal Width')
+plt.show()
+
+correlation = np.corrcoef(X, y)[0,1]
+print(f"Correlation coefficient between Petal Length and Petal Width: {correlation}")
+model = LinearRegression()
+
+X_reshaped = X.values.reshape(-1,1)
+
+model.fit(X_reshaped, y)
+
+y_pred= model.predict(X_reshaped)
+
+plt.figure(figsize=(8,6))
+plt.scatter(X, y, color="blue", label="Actual Data")
+plt.plot(X,y_pred,color='red', label='Regression Line')
+plt.xlabel('Petal Length')
+plt.ylabel("Prtal Width")
+plt.title("Linear Regression: Petal Lenght vs Petal Width")
+plt.legend()
+plt.show()
+print(f"Slope (Coefficient): {model.coef_[0]}")
+
+print(f"Intercept: {model.intercept_}")
         `
 
 func Lab1Code(c *gin.Context) {
-	c.Data(200, "text/plain", []byte(code))
+	c.Data(200, "text/plain", []byte(code1))
 }
--- main.go
+++ main.go
@@ -12,6 +12,15 @@ func main() {
 	v1 := router.Group("/ai_ethics")
 	{
 		v1.GET("/1", aiethics.Lab1Code)
+		v1.GET("/2", aiethics.Lab1Code)
+		v1.GET("/3", aiethics.Lab1Code)
+		v1.GET("/4", aiethics.Lab1Code)
+		v1.GET("/5", aiethics.Lab1Code)
+		v1.GET("/6", aiethics.Lab1Code)
+		v1.GET("/7", aiethics.Lab1Code)
+		v1.GET("/8", aiethics.Lab1Code)
+		v1.GET("/9", aiethics.Lab1Code)
+		v1.GET("/10", aiethics.Lab1Code)
 	}
 
 	router.Run(":8080")
